e3ccae6198843caa2e0b47a8d753c451a3578c57
yanpeng <yanpeng@git.com>
2019-10-24 18:32:28 +0800

    创建项目

---
+++ src/main/java/com/yanpeng/shijie/ShijieApplication.java
@@ -0,0 +1,13 @@
+package com.yanpeng.shijie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShijieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShijieApplication.class, args);
+    }
+
+}
